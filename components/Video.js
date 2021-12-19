1e442d5d02bc28b4c9ff807ae08c52a92d6fb98c
UnicornStudio <40837337+UnicornStudio@users.noreply.github.com>
2021-12-19 14:48:05 -0700

    Changed video

--- components/Video.js
+++ components/Video.js
@@ -1,7 +1,7 @@
 const Video = () => {
     return (
         <div className="video-trailer">
-            <iframe className="video" src="https://www.youtube.com/embed/5JPLdEd3oKo" title="YouTube video player" frameBorder="0" style={{"border" : "0"}} allowFullScreen></iframe>
+            <iframe className="video" src="https://www.youtube.com/embed/qdXh66tJyGw" title="YouTube video player" frameBorder="0" style={{"border" : "0"}} allowFullScreen></iframe>
         </div>
     )
 }
